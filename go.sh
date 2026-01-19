0fc58a9cbe9d6c10cb4802f9011806a6aaba0739
Craig McNeile <craig.mcneile@plymouth.ac.uk>
2026-01-19 19:46:19 +0000

    update

--- go.sh
+++ go.sh
@@ -2,10 +2,12 @@
 
 ./clean.sh
 
-./fit.py ./sim_data/g1_model.yaml
+#./fit.py ./sim_data/g1_model.yaml
 #./fit.py ./sim_data/g3_model.yaml
 
 #./fit.py ../formfactor/etmc/ga_etmc.yaml
+
+./fit.py data/ga_etmc.yaml
 
 echo "Job run at "
 date
